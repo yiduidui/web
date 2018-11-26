96abb49cab0f725651887a25d6b07fe22653aeb3
yiduidui <45277675+yiduidui@users.noreply.github.com>
2018-11-26 16:29:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+[{
+    "squadName" : "Super hero squad",
+    "homeTown" : "Metro City",
+    "formed" : 2016,
+    "secretBase" : "Super tower",
+    "active" : true,
+    "members" : [
+      {
+        "姓名" : "易烊千玺",
+        "出生日期" : 2000.11.28
+        "secretIdentity" : "Dan Jukes",
+        "powers" : [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast"
+        ]
+      },
+      {
+        "name" : "Madame Uppercut",
+        "age" : 39,
+        "secretIdentity" : "Jane Wilson",
+        "powers" : [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name" : "Eternal Flame",
+        "age" : 1000000,
+        "secretIdentity" : "Unknown",
+        "powers" : [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  }]
+  var myJSON = { "name" : "Chris", "age" : "38" };
+  myJSON
+  var myString = JSON.stringify(myJSON);
+  myString
